3103c0e9dc3081f30fd3fd0fca6fc93f841b21f9
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-10-25 17:08:28 +0330

    Add project files.

---
+++ Linq examples/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+
+
+
+
+
+string[] names = { "Mehrshad", "Ahmad", "Mehdi", "Amir", "Sheida", "Rozhin" };
+
+// query synrax
+
+//var result = from name in names
+//             where name.EndsWith("d")
+//             select name;
+//foreach (var name in result)
+//{
+//    Console.WriteLine(name);
+//}
+//-----------------------------------------------------------------
+
+// method syntax
+
+//var result = names.Where(name => name.EndsWith("d"));
+//foreach (var name in result)
+//{
+//    Console.WriteLine(name);
+//}
+//-----------------------------------------------------------------
+
+// mixed syntax
+
+//var result = (from name in names
+//              where name.StartsWith("M")
+//              select name).Reverse();
+//foreach (var item in result)
+//{
+//    Console.WriteLine(item);
+//}
---
+++ Linq examples/ex2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_examples
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Family { get; set; }
+    }
+}
