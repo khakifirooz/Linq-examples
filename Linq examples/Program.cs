7a8900c2ddbbc10962c30a2be2312a55958da96c
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-10-27 21:29:55 +0330

    filterig where

--- Linq examples/Program.cs
+++ Linq examples/Program.cs
@@ -1,44 +1,21 @@
-﻿
-using Linq_examples;
+﻿// filterfing ( Where , OfType)
 
-List<Student> students = new List<Student>()
-{
-    new Student {Id = 00001, Name = "mehrshad", Age =21 , Family = "khaki"},
-    new Student {Id = 00002, Name = "amir", Age =22, Family = "kochaki"},
-    new Student {Id = 00003, Name = "ahmad", Age =21, Family = "heidari"},
-    new Student {Id = 00004, Name = "mehdi", Age =22, Family = "ahmadvand"}
-};                           
+string[] names = { "mohammad", " ali", "reza", "nima", "akram", "zahra" };
 
-// where 
-var query = students.Where(x => x.Name == "mehrshad");
-
+var query = names.Where(x => CheckLenght(x));
 foreach (var item in query)
 {
-    Console.WriteLine($"the id is {item.Id} and his age is {item.Age} , name is {item.Name}");
-}
-
-// select
-var query2 = students.Select(x => new Student
-{
-    Age = x.Age,
-    Name = x.Name
-});
-foreach (var item in query2)
-{
-   Console.WriteLine($"name is {item.Name} , age is = {item.Age}");
+    Console.WriteLine(item);
 }
 
 
-// annonymouse
-var query3 = students.Select(x => new
+static bool CheckLenght(string str)
 {
-    Id = x.Id,
-    fullname = $"{x.Name} {x.Family} "
-});
-foreach (var item in query3)
-{
-    Console.WriteLine($"full name is: {item.fullname} and Id : {item.Id}");
+    if (str.Length > 5)   // mohammad is the output
+        return true;
+    return false;
 }
+
 Console.ReadKey();
 
 
@@ -53,7 +30,63 @@ Console.ReadKey();
 
 
 
-string[] names = { "Mehrshad", "Ahmad", "Mehdi", "Amir", "Sheida", "Rozhin" };
+
+
+//using Linq_examples;
+
+//List<Student> students = new List<Student>()
+//{
+//    new Student {Id = 00001, Name = "mehrshad", Age =21 , Family = "khaki"},
+//    new Student {Id = 00002, Name = "amir", Age =22, Family = "kochaki"},
+//    new Student {Id = 00003, Name = "ahmad", Age =21, Family = "heidari"},
+//    new Student {Id = 00004, Name = "mehdi", Age =22, Family = "ahmadvand"}
+//};                           
+
+//// where 
+//var query = students.Where(x => x.Name == "mehrshad");
+
+//foreach (var item in query)
+//{
+//    Console.WriteLine($"the id is {item.Id} and his age is {item.Age} , name is {item.Name}");
+//}
+
+//// select
+//var query2 = students.Select(x => new Student
+//{
+//    Age = x.Age,
+//    Name = x.Name
+//});
+//foreach (var item in query2)
+//{
+//   Console.WriteLine($"name is {item.Name} , age is = {item.Age}");
+//}
+
+
+//// annonymouse
+//var query3 = students.Select(x => new
+//{
+//    Id = x.Id,
+//    fullname = $"{x.Name} {x.Family} "
+//});
+//foreach (var item in query3)
+//{
+//    Console.WriteLine($"full name is: {item.fullname} and Id : {item.Id}");
+//}
+//Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+//string[] names = { "Mehrshad", "Ahmad", "Mehdi", "Amir", "Sheida", "Rozhin" };
 
 // query synrax
 
